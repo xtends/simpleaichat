d18fdff7e7330e32c258710f28f4823a43d61c13
codephreak <140855987+Professor-Codephreak@users.noreply.github.com>
2023-09-04 19:37:15 -0700

    Update schat.py
    Explanation
    
        load_environment(): This function handles the loading of environment variables.
        validate_api_key(api_key): Validates the API key using a simple regex. You can customize the validation as needed.
        store_api_key(): Asks the user for the API key and stores it only if it's valid.
        initialize_chat(api_key): Initializes the AIChat and handles any exceptions that may arise during initialization.
        main(): The main function that orchestrates the overall flow.

--- schat.py
+++ schat.py
@@ -1,34 +1,57 @@
 from simpleaichat import AIChat
 from dotenv import load_dotenv, set_key
 import os
+import re
 
-# Load existing environment variables from .env file
-load_dotenv()
+def load_environment():
+    load_dotenv()
+
+def validate_api_key(api_key):
+    # Simple validation using regex (customize as needed)
+    return bool(re.match(r'^[a-zA-Z0-9-_]+$', api_key))
 
 def store_api_key():
-    api_key = input("Enter your OpenAI API key: ")
-    set_key('.env', 'OPENAI_API_KEY', api_key)
+    while True:
+        api_key = input("Enter your OpenAI API key: ")
+        if validate_api_key(api_key):
+            set_key('.env', 'OPENAI_API_KEY', api_key)
+            break
+        else:
+            print("Invalid API key. Please enter a valid key.")
+
+def initialize_chat(api_key):
+    try:
+        return AIChat(api_key=api_key)
+    except Exception as e:
+        print(f"Failed to initialize AIChat: {e}")
+        return None
 
 def main():
-    # Check if API key is already stored
+    load_environment()
+
     api_key = os.getenv("OPENAI_API_KEY")
     
     if api_key is None:
         print("API key not found.")
         store_api_key()
-        load_dotenv()  # Reload .env to get the newly stored API key
+        load_environment()
         api_key = os.getenv("OPENAI_API_KEY")
 
-    # Initialize the AIChat
-    ai = AIChat(api_key=api_key)
+    ai = initialize_chat(api_key)
+    if ai is None:
+        print("Exiting.")
+        return
 
     while True:
         user_input = input("You: ")
         if user_input.lower() == 'quit':
             print("Exiting chat.")
             break
-        response = ai(user_input)
-        print(f"AI: {response}")
+        try:
+            response = ai(user_input)
+            print(f"AI: {response}")
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     main()
